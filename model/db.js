f6151570b7b2e63a7a2cabadaaca93d05a11a059
Barak Stout <bstout9@gatech.edu>
2018-12-05 11:39:56 -0500

    minor edit

--- model/db.js
+++ model/db.js
@@ -4,10 +4,11 @@ var mysql = require('mysql');
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
+    host     : '192.168.117.129',
     user     : 'root',
     password : '1234',
-    database : 'QADB',
+    database : 'QADB_DB',
+    port: 80
 });
 
 connection.connect(function(err) {
